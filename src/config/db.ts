af602a3dfb7b9dc6dac5d845201677f5fee3a646
WalidurTanjim <mohammadtanjim92@gmail.com>
2026-01-31 11:29:47 +0600

    db.ts file of config created & working well

---
+++ src/config/db.ts
@@ -0,0 +1,46 @@
+import { Pool } from "pg";
+import config from ".";
+
+// Database
+export const pool = new Pool({
+    connectionString: `${config?.connection_string}`
+});
+
+const initDB = async() => {
+    try{
+        await pool.query("BEGIN");
+
+        await pool.query(`
+                CREATE TABLE IF NOT EXISTS users(
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(100) NOT NULL,
+                    email VARCHAR(150) UNIQUE NOT NULL,
+                    age INT,
+                    phone VARCHAR(15),
+                    address TEXT,
+                    created_at TIMESTAMP DEFAULT NOW(),
+                    updated_at TIMESTAMP DEFAULT NOW()
+                )
+            `);
+
+        await pool.query(`
+                CREATE TABLE IF NOT EXISTS todos(
+                    id SERIAL PRIMARY KEY,
+                    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+                    title VARCHAR(200) NOT NULL,
+                    description TEXT,
+                    completed BOOLEAN DEFAULT FALSE,
+                    due_date DATE,
+                    created_on TIMESTAMP DEFAULT NOW(),
+                    updated_on TIMESTAMP DEFAULT NOW()
+                )
+            `);
+
+        await pool.query("COMMIT");
+    }catch(err){
+        await pool.query("ROLLBACK");
+        console.error("❌ Database not initialized!", err);
+    }
+};
+
+export default initDB;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
-import { Pool } from "pg";
 import config from "./config";
+import initDB, { pool } from "./config/db";
 
 const app = express();
 const port = config?.port;
@@ -8,48 +8,7 @@ const port = config?.port;
 // parser
 app.use(express.json());
 
-// Database
-const pool = new Pool({
-    connectionString: `${config?.connection_string}`
-});
-
-const initDB = async() => {
-    try{
-        await pool.query("BEGIN");
-
-        await pool.query(`
-                CREATE TABLE IF NOT EXISTS users(
-                    id SERIAL PRIMARY KEY,
-                    name VARCHAR(100) NOT NULL,
-                    email VARCHAR(150) UNIQUE NOT NULL,
-                    age INT,
-                    phone VARCHAR(15),
-                    address TEXT,
-                    created_at TIMESTAMP DEFAULT NOW(),
-                    updated_at TIMESTAMP DEFAULT NOW()
-                )
-            `);
-
-        await pool.query(`
-                CREATE TABLE IF NOT EXISTS todos(
-                    id SERIAL PRIMARY KEY,
-                    user_id INT REFERENCES users(id) ON DELETE CASCADE,
-                    title VARCHAR(200) NOT NULL,
-                    description TEXT,
-                    completed BOOLEAN DEFAULT FALSE,
-                    due_date DATE,
-                    created_on TIMESTAMP DEFAULT NOW(),
-                    updated_on TIMESTAMP DEFAULT NOW()
-                )
-            `);
-
-        await pool.query("COMMIT");
-    }catch(err){
-        await pool.query("ROLLBACK");
-        console.error("❌ Database not initialized!", err);
-    }
-};
-
+// Initialize database
 initDB();
 
 // API routes starts
