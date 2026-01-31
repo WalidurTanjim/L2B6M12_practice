29f1fcde11bcf9ac25121eda885b5f3faa1f5fd9
WalidurTanjim <mohammadtanjim92@gmail.com>
2026-01-31 17:00:20 +0600

    delete single user by id from modulers

--- src/server.ts
+++ src/server.ts
@@ -22,69 +22,6 @@ app.get("/", logger, (req: Request, res: Response) => {
 // users table API routes
 app.use("/users", userRoutes);
 
-// app.put("/users/:id", async(req: Request, res: Response) => {
-//     const { id } = req?.params;
-//     const { name, email, age, phone, address } = req?.body;
-
-//     try{
-//         const result = await pool.query(`UPDATE users SET name=$1, email=$2, age=$3, phone=$4, address=$5 WHERE id = $6 RETURNING *`, [name, email, age, phone, address, id]);
-
-//         // console.log("Updated user:", result?.rows);
-
-//         if(result?.rows.length === 0){
-//             res.status(400).json({
-//                 success: false, 
-//                 message: "User Not Found"
-//             });
-//         }else{
-//             res.status(200).json({
-//                 success: true,
-//                 message: "User Updated Successfully",
-//                 data: result?.rows
-//             });
-//         }
-//     }catch(err: any){
-//         console.error(err?.message);
-
-//         res.status(500).json({
-//             success: false,
-//             message: err?.message,
-//             details: err
-//         });
-//     }
-// })
-
-app.delete("/users/:id", async(req: Request, res: Response) => {
-    const { id } = req?.params;
-
-    try{
-        const result = await pool.query(`DELETE FROM users WHERE id = $1`, [id]);
-        
-        // console.log("✔ User Deleted Successfully!", result?.rowCount);
-
-        if(result?.rowCount === 0){
-            res.status(400).json({
-                success: false, 
-                message: "User Now Found!"
-            });
-        }else{
-            res.status(200).json({
-                success: true, 
-                message: "✔ User Deleted Successfully.",
-                data: result?.rowCount
-            });
-        }
-    }catch(err: any){
-        console.error(err?.message);
-
-        res.status(500).json({
-            success: false,
-            message: "User Not Found!",
-            details: err
-        });
-    }
-})
-
 
 
 // todos table API route 
